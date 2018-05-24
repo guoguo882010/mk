424d4e42c695d466b6c2880ab681c295d6edc52a
gk <guoguo882010@hotmail.com>
2018-05-24 11:18:46 +0800

    增加rem的css，js

---
+++ rem.js
@@ -0,0 +1,22 @@
+/*使用rem布局 必须导入
+* 12px = 12/100 = 0.12rem
+* 14px = 0.14rem
+* 16px = 0.16rem
+* */
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if(clientWidth>=640){
+                docEl.style.fontSize = '100px';
+            }else{
+                docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+            }
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
